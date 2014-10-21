dafad753bfdf4a5e7ab6b992e03c459541166f81
Nicolas Beauvais <nicolas@bvs.email>
2014-10-21 17:31:27 +0200

    Update db scheme with mobile id

---
+++ app/database/migrations/2014_10_21_165259_create_mobiles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMobilesTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mobiles', function($table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('mobile_id', 250);
+            $table->dateTime('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('mobiles');
+    }
+
+}
